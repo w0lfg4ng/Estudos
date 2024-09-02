44caec4584732658947012e5c88ad5f55ea1e934
wolfgang <161239309+w0lfg4ng@users.noreply.github.com>
2024-09-02 20:55:54 -0300

    Update temporizador.asm

--- ASSEMBLY 8051/temporizador.asm
+++ ASSEMBLY 8051/temporizador.asm
@@ -1,3 +1,8 @@
+;CODE BY M0RCEG0 P0CILGA
+
+;TEMPORIZADOR SEM INICIALIZADOR DE TIMER
+
+
 timer:      mov     timer,#(16-7)    ;conta 7us
             setb    liga             ;liga timer
 
@@ -6,3 +11,14 @@ novo_tempo: jnb    TF,$              ;espera overflow
             mov    timer,#(16-7)     ;incrementa valor para recarregar
 
             jmp    novo_tempo
+
+;TEMPORIZADOR SEM INICIALIZADOR DE TIMER
+
+timer:            mov            TL0,#18H            ;inicializa timer low
+                  mov            TH0,#63h            ;inicializa timer low
+                  setb           TR0                 ;liga o timer
+denovo:           jb             TF0,$               
+                  clr            TF0
+                  mov            TH0,#(256-156)
+
+                  jmp            denovo            
